6a749cb23410e65164d666afd5c3d0c28193bec8
darman1725 <darmansaragih1701@gmail.com>
2023-04-10 08:42:02 +0700

    Menambahkan menu searching data dan show data berdasarkan jumlah

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -10,23 +10,30 @@
     </div>
 
 <!-- Bootstrap core JavaScript-->
-  <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  
-  <!-- Core plugin JavaScript-->
-  <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
-  <!-- Page level plugins -->
-  <script src="{{ asset('assets/vendor/datatables/jquery.dataTables.min.js') }}"></script>
-  <script src="{{ asset('assets/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
-  
-  <script>
-  $(function () {
-		$('[data-toggle="tooltip"]').tooltip()
-  })
-  </script>
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('assets/vendor/chart.js/Chart.min.js') }}"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('assets/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{ asset('assets/js/demo/datatables-demo.js') }}"></script>
+
+<script>
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
+
 
 </footer>
--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -1,3 +1,16 @@
+<head>
+<!-- Custom fonts for this template-->
+<link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css"> 
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+{{-- <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet"> --}}
+
+<link href="{{ asset('assets/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+<link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon">
+<link rel="icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon">
+</head>
+
 <header class='mb-3'>
     <nav class="navbar navbar-expand navbar-light ">
         <div class="container-fluid">
