2f24bba107c8ec929f0115c5e7ebd117ef8e667b
darman1725 <darmansaragih1701@gmail.com>
2023-06-07 18:07:05 +0700

    Membuat perangkingan peserta by kegiatan

--- app/Http/Controllers/Management/DataHasilAkhirController.php
+++ app/Http/Controllers/Management/DataHasilAkhirController.php
@@ -3,22 +3,84 @@
 namespace App\Http\Controllers\Management;
 
 use App\Http\Controllers\Controller;
+use App\Models\Menu\DataKegiatan;
+use App\Models\Menu\Pendaftaran;
+use App\Models\Menu\DataKriteria;
 use App\Models\Management\DataPerhitungan;
 use App\Models\Management\DataHasilAkhir;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use PDF;
 
 class DataHasilAkhirController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $selectedKegiatan = $request->input('kegiatan');
+        $kriteria = DataKriteria::with('kegiatan')->get();
+        $pendaftaran = DataPerhitungan::get_pendaftaran();
+    
+        if ($selectedKegiatan) {
+            // Filter pendaftaran based on selected kegiatan
+            $filteredPendaftaran = collect($pendaftaran)->filter(function ($item) use ($selectedKegiatan) {
+                return $item->id_data_kegiatan == $selectedKegiatan;
+            });
+    
+            $pendaftaranIds = $filteredPendaftaran->pluck('id')->toArray();
+    
+            $pendaftaran = Pendaftaran::with('user')
+                ->whereIn('id', $pendaftaranIds)
+                ->get();
+    
+            // Filter the kriteria based on selected kegiatan
+            $kriteria = $kriteria->filter(function ($item) use ($selectedKegiatan) {
+                return $item->kegiatan->id == $selectedKegiatan;
+            });
+    
+            // Get the kode_kriteria values related to the selected kegiatan
+            $kodeKriteria = $kriteria->pluck('kode_kriteria')->toArray();
+    
+            // Get min and max values for each kode_kriteria from the filtered pendaftaran data
+            $minMaxValues = [];
+            foreach ($kodeKriteria as $kode) {
+                $minMaxValues[$kode] = [
+                    'min' => $pendaftaran->min("nilai_{$kode}"),
+                    'max' => $pendaftaran->max("nilai_{$kode}")
+                ];
+            }
+        } else {
+            $pendaftaranIds = Arr::pluck($pendaftaran, 'id');
+    
+            $pendaftaran = Pendaftaran::with('user')
+                ->whereIn('id', $pendaftaranIds)
+                ->get();
+    
+            $kodeKriteria = $kriteria->pluck('kode_kriteria')->toArray();
+            $minMaxValues = []; // Initialize an empty array for minMaxValues
+        }
+    
+        // Get distinct kegiatan options for filtering
+        $kegiatanOptions = DataKegiatan::all();
+    
         $data = [
             'page' => "Perhitungan",
-            'kriteria'=> DataPerhitungan::get_kriteria(),
-            'pendaftaran'=> DataPerhitungan::get_pendaftaran(),
+            'kriteria' => $kriteria,
+            'pendaftaran' => $pendaftaran,
+            'selectedKegiatan' => $selectedKegiatan,
+            'kodeKriteria' => $kodeKriteria, // Pass the filtered kode_kriteria values to the view
+            'kegiatanOptions' => $kegiatanOptions,
+            'minMaxValues' => $minMaxValues // Add minMaxValues to the data array
         ];
-
-        return view('management.data_hasil_akhir.index', $data);
+    
+        // Hitung total bobot
+        $totalBobot = $kriteria->sum('bobot'); // Anda perlu menyesuaikan nama kolom bobot sesuai dengan struktur tabelnya
+    
+        // Tambahkan totalBobot ke dalam data
+        $data['totalBobot'] = $totalBobot;
+    
+        return view('management.data_hasil_akhir.index', $data, [
+            'selectedKegiatan' => $request->input('kegiatan'),
+        ]);
     }
     
     public function generatePDF(Request $request)
--- resources/views/management/data_hasil_akhir/index.blade.php
+++ resources/views/management/data_hasil_akhir/index.blade.php
@@ -2,96 +2,126 @@
 
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-fw fa-chart-area"></i> Data Hasil Akhir</h1>
-
-        <a href="{{ route('generate_pdf') }}" class="btn btn-primary"> <i class="fa fa-print"></i> Cetak
-            Data </a>
     </div>
 
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary"><i class="fa fa-table"></i> Daftar Urutan Rangking</h6>
+    <form action="{{ route('data_hasil_akhir') }}" method="GET">
+        <div class="form-group">
+            <label for="kegiatan">Filter Kegiatan:</label>
+            <select name="kegiatan" id="kegiatan" class="form-control">
+                <option value="">-- Pilih Kegiatan --</option>
+                @foreach($kegiatanOptions as $option)
+                <option value="{{ $option->id }}" {{ $selectedKegiatan==$option->id ? 'selected' : '' }}>
+                    {{ $option->nama }} - {{ $option->jenis }}
+                </option>
+                @endforeach
+            </select>
         </div>
+        <button type="submit" class="btn btn-primary">Filter</button>
+    </form>
+    <br>
 
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" width="100%" cellspacing="0">
-                    <thead class="bg-primary text-white">
-                        <tr align="center">
-                            <th width="5%">No</th>
-                            <th>Nama Pelamar</th>
-                            <th>Kegiatan</th>
-                            <th>Total Nilai</th>
-                            <th width="5%">Rangking</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                    $no = 1;
-                    $total_bobot = \App\Models\Management\DataPerhitungan::get_total_kriteria();
-                    $pendaftaran = \App\Models\Menu\Pendaftaran::with(['user' => function ($query) {
-                    $query->orderBy('nama_lengkap', 'asc');
-                    }])->get();
-                    $pendaftaran_data = array();
-                    foreach ($pendaftaran as $keys) {
-                        $nilai_total = 0;
-                        foreach ($kriteria as $key) {
-                            $data_pencocokan = \App\Models\Management\DataPerhitungan::data_nilai($keys->id, $key->id);
-                            $min_max = \App\Models\Management\DataPerhitungan::get_max_min($key->id);
-    
-                            if ($total_bobot['total_bobot'] != 0) {
-                                $bobot_normalisasi = $key->bobot / $total_bobot['total_bobot'];
-                            } else {
-                                $bobot_normalisasi = 0;
-                            }
-    
-                            if ($min_max && $min_max['max'] != $min_max['min']) {
-                                if ($key->jenis == "Benefit") {
-                                    $nilai_utility = @(($data_pencocokan->nilai - $min_max['min']) / ($min_max['max'] - $min_max['min']));
+    @if(isset($totalBobot) && $totalBobot < 100) <div class="alert alert-danger" role="alert"><i
+            class="bi bi-info-circle"></i>
+        Proses perhitungan belum dapat dilanjutkan pada kegiatan ini. Dikarenakan data masih belum lengkap.<br>
+        Silahkan lengkapi data pada menu perhitungan, agar data kegiatan ini memenuhi kaidah perhitungan.
+        </div>
+        <a href="{{ route('data_perhitungan') }}" class="btn btn-success btn-icon-split"><span
+                class="icon text-white-50"><i class="fas fa-arrow-left"></i></span>
+            <span class="text">Kembali ke menu data perhitungan</span>
+        </a>
+        @else
+        <div class="card shadow mb-4">
+            <!-- /.card-header -->
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary"><i class="fa fa-table"></i> Perhitungan Nilai</h6>
+            </div>
+
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" width="100%" cellspacing="0">
+                        <thead class="bg-primary text-white">
+                            <tr align="center">
+                                <th width="5%">No</th>
+                                <th>Alternatif</th>
+                                <th>Kegiatan</th>
+                                <th width="15%">Total Nilai</th>
+                                <th>Rangking</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(empty($selectedKegiatan))
+                            <tr>
+                                <td colspan="3">Tidak ada kegiatan yang dipilih.</td>
+                            </tr>
+                            @else
+                            <?php
+                        $no = 1;
+                        $data = [];
+                        $total_bobot = \App\Models\Management\DataPerhitungan::get_total_kriteria();
+                        foreach ($pendaftaran as $keys) {
+                            $nilai_total = 0;
+                            foreach ($kriteria as $key) {
+                                $data_pencocokan = \App\Models\Management\DataPerhitungan::data_nilai($keys->id, $key->id);
+                                $min_max = \App\Models\Management\DataPerhitungan::get_max_min($key->id);
+
+                                if ($total_bobot['total_bobot'] != 0) {
+                                    $bobot_normalisasi = $key->bobot / $totalBobot ?? 0;
                                 } else {
-                                    $nilai_utility = @(($min_max['max'] - $data_pencocokan->nilai) / ($min_max['max'] - $min_max['min']));
+                                    $bobot_normalisasi = 0;
                                 }
-                            } else {
-                                $nilai_utility = 0;
+
+                                if ($min_max && $min_max['max'] != $min_max['min']) {
+                                    if ($key->jenis == "Benefit") {
+                                        $nilai_utility = @(($data_pencocokan->nilai - $min_max['min']) / ($min_max['max'] - $min_max['min']));
+                                    } else {
+                                        $nilai_utility = @(($min_max['max'] - $data_pencocokan->nilai) / ($min_max['max'] - $min_max['min']));
+                                    }
+                                } else {
+                                    $nilai_utility = 0;
+                                }
+
+                                $nilai_total += $bobot_normalisasi * $nilai_utility;
                             }
-    
-                            $nilai_total += $bobot_normalisasi * $nilai_utility;
+                            $data[] = [
+                                'id_pendaftaran' => $keys->user->nama_lengkap,
+                                'jenis_kegiatan' => $keys->kegiatan->nama,
+                                'nilai' => $nilai_total
+                            ];
                         }
-                        $pendaftaran_data[] = [
-                            'id' => $keys->id,
-                            'nama' => $keys->user->nama_lengkap,
-                            'kegiatan' => $keys->kegiatan->nama,
-                            'nilai_total' => $nilai_total
-                        ];
-                    }
-    
-                    // Urutkan data alternatif berdasarkan nilai total secara descending
-                    $pendaftaran_data = collect($pendaftaran_data)->sortByDesc('nilai_total')->values()->all();
-    
-                    foreach ($pendaftaran_data as $keys) {
-                    ?>
-                        <tr align="center">
-                            <td>
-                                <?= $no++; ?>
-                            </td>
-                            <td align="left">
-                                <?= $keys['nama']; ?>
-                            </td>
-                            <td>
-                                <?= $keys['kegiatan']; ?>
-                            </td>
-                            <td>
-                                <?= $keys['nilai_total']; ?>
-                            </td>
-                            <td>
-                                <?= $no-1; ?>
-                            </td>
-                        </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+
+                        // Sort the data based on 'nilai' in descending order
+                        usort($data, function ($a, $b) {
+                            return $b['nilai'] <=> $a['nilai'];
+                        });
+
+                        foreach ($data as $index => $item) {
+                            ?>
+                            <tr align="center">
+                                <td>
+                                    {{ $no }}
+                                </td>
+                                <td align="left">
+                                    {{ $item['id_pendaftaran'] }}
+                                </td>
+                                <td>
+                                    {{ $item['jenis_kegiatan'] }}
+                                </td>
+                                <td>
+                                    {{ $item['nilai'] }}
+                                </td>
+                                <td>
+                                    {{ $index + 1 }}
+                                </td>
+                            </tr>
+                            <?php
+                            $no++;
+                        }
+                        ?>
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-
-
+        @endif
 </x-app-layout>
