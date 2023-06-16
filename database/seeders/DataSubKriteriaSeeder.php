6b95dda653916cb7c1fab4a4709f76d7683c035b
darman1725 <darmansaragih1701@gmail.com>
2023-06-16 15:25:44 +0700

    Mengubah susunan kriteria dan sub kriteria

--- database/seeders/DataSubKriteriaSeeder.php
+++ database/seeders/DataSubKriteriaSeeder.php
@@ -43,130 +43,130 @@ class DataSubKriteriaSeeder extends Seeder
             'nilai' => 100,
         ],
 
-        // Data sub kriteria dari REG 2 - Pengalaman
+        // Data sub kriteria dari REG 2 - Pendidikan
         [
             'id_data_kegiatan' => 1,
             'id_data_kriteria' => 2,
-            'deskripsi' => '6 Survei',
-            'nilai' => 100,
-        ],
-        [
-            'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 2,
-            'deskripsi' => '5 Survei',
-            'nilai' => 95,
-        ],
-        [
-            'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 2,
-            'deskripsi' => '4 Survei',
-            'nilai' => 90,
-        ],
-        [
-            'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 2,
-            'deskripsi' => '3 Survei',
-            'nilai' => 85,
-        ],
-        [
-            'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 2,
-            'deskripsi' => '2 Survei',
-            'nilai' => 80,
-        ],
-        [
-            'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 2,
-            'deskripsi' => '1 Survei',
-            'nilai' => 75,
-        ],
-        [
-            'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 2,
-            'deskripsi' => '0 Survei',
-            'nilai' => 70,
-        ],
-
-        // Data sub kriteria dari REG 3 - Pendidikan
-        [
-            'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 3,
             'deskripsi' => 'S2',
             'nilai' => 100,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 3,
+            'id_data_kriteria' => 2,
             'deskripsi' => 'D4/S1',
             'nilai' => 95,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 3,
+            'id_data_kriteria' => 2,
             'deskripsi' => 'D1/D2/D3',
             'nilai' => 90,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 3,
+            'id_data_kriteria' => 2,
             'deskripsi' => 'SMA',
             'nilai' => 85,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 3,
+            'id_data_kriteria' => 2,
             'deskripsi' => 'SMP',
             'nilai' => 80,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 3,
+            'id_data_kriteria' => 2,
             'deskripsi' => 'SD',
             'nilai' => 75,
         ],
 
-        // Data sub kriteria dari REG 4 - Pekerjaan
+        // Data sub kriteria dari REG 3 - Pekerjaan
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 4,
+            'id_data_kriteria' => 3,
             'deskripsi' => 'Kader PKK/Karang Taruna',
             'nilai' => 70,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 4,
+            'id_data_kriteria' => 3,
             'deskripsi' => 'Aparat Desa/Kelurahan',
             'nilai' => 75,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 4,
+            'id_data_kriteria' => 3,
             'deskripsi' => 'Mengurus Rumah Tangga',
             'nilai' => 80,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 4,
+            'id_data_kriteria' => 3,
             'deskripsi' => 'Pelajar/Mahasiswa',
             'nilai' => 85,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 4,
+            'id_data_kriteria' => 3,
             'deskripsi' => 'Wiraswasta',
             'nilai' => 90,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 4,
+            'id_data_kriteria' => 3,
             'deskripsi' => 'Pegawai/Guru Honorer',
             'nilai' => 95,
         ],
         [
             'id_data_kegiatan' => 1,
-            'id_data_kriteria' => 4,
+            'id_data_kriteria' => 3,
             'deskripsi' => 'Lainnya',
             'nilai' => 100,
+        ],
+
+        // Data sub kriteria dari REG 4 - Pengalaman
+        [
+            'id_data_kegiatan' => 1,
+            'id_data_kriteria' => 4,
+            'deskripsi' => '6 Survei',
+            'nilai' => 100,
+        ],
+        [
+            'id_data_kegiatan' => 1,
+            'id_data_kriteria' => 4,
+            'deskripsi' => '5 Survei',
+            'nilai' => 95,
+        ],
+        [
+            'id_data_kegiatan' => 1,
+            'id_data_kriteria' => 4,
+            'deskripsi' => '4 Survei',
+            'nilai' => 90,
+        ],
+        [
+            'id_data_kegiatan' => 1,
+            'id_data_kriteria' => 4,
+            'deskripsi' => '3 Survei',
+            'nilai' => 85,
+        ],
+        [
+            'id_data_kegiatan' => 1,
+            'id_data_kriteria' => 4,
+            'deskripsi' => '2 Survei',
+            'nilai' => 80,
+        ],
+        [
+            'id_data_kegiatan' => 1,
+            'id_data_kriteria' => 4,
+            'deskripsi' => '1 Survei',
+            'nilai' => 75,
+        ],
+        [
+            'id_data_kegiatan' => 1,
+            'id_data_kriteria' => 4,
+            'deskripsi' => '0 Survei',
+            'nilai' => 70,
         ],
 
         // Data sub kriteria dari REG 5 - Kepemilikan Gadget
