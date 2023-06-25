3bbd1327afe870c93df6bd962d0b1ff6e2eb7fd6
dhomanhurimtm <dhomanhuri@mtm.id>
2023-06-26 06:10:00 +0700

    change folder

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,14 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-            'resources/css/app.css',
-            'resources/css/auth.css', 
-            'resources/js/app.js',
-        ],
-        refresh: true,
+                'resources/css/app.css',
+                'resources/css/auth.css',
+                'resources/js/app.js',
+                'resources/images/logo/favicon.svg',
+                'resources/images/logo/favicon.png',
+                'resources/images/logo/favicon2.png',
+            ],
+            refresh: true,
         }),
     ],
 });
